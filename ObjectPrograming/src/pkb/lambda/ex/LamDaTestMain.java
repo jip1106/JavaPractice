8cd2e7f4b91acb64fff5be8272b1ce62a9d435b9
Q220135 <Q220135@Q220135-PC>
2022-01-17 16:25:40 +0900

    컬렉션 ArrayList 사용법

---
+++ ObjectPrograming/src/pkb/lambda/ex/LamDaTestMain.java
@@ -0,0 +1,13 @@
+package pkb.lambda.ex;
+
+public class LamDaTestMain {
+
+	public static void main(String[] args) {
+
+		/*
+			람다를 이용하면 어떤 동작을 Parameter으로 만들 수 있다. (함수의 인자로 함수를 받을 수 있다)
+		 */
+		
+	}
+
+}
--- ObjectPrograming/src/pkg/collection/main/CollectionMain.java
+++ ObjectPrograming/src/pkg/collection/main/CollectionMain.java
@@ -1,6 +1,9 @@
 package pkg.collection.main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 import pkg.collection.list.VectorArrayListForEx;
 
@@ -8,10 +11,10 @@ public class CollectionMain {
 
 	public static void main(String[] args) {
 		VectorArrayListForEx vaList = null;
-		
+		int vaListSize = 3;
 		ArrayList<ArrayList<String>> listArr = new ArrayList<ArrayList<String>>();
 		
-		for(int i=0; i<3; i++) {
+		for(int i=0; i<vaListSize; i++) {
 			ArrayList<String> inList = new ArrayList<String>();
 			
 			for(int j=0; j<=i; j++) {
@@ -22,32 +25,113 @@ public class CollectionMain {
 		}
 		
 		vaList = new VectorArrayListForEx(listArr.size());
-		//System.out.println(listArr);
 		vaList.setListArr(listArr);
 		
 		vaList.print(vaList, vaList.getSize(),"f");
 		vaList.print(vaList, vaList.getSize(),"e");
 		vaList.print(vaList, vaList.getSize(),"l");
 		
+		/*ArrayList => 순서가 있는 데이터 중복이 가능한 컬렉션 */
+		ArrayList<Object> list = new ArrayList<Object>();
+
+		// add(value) => value 추가
+		list.add("서울");		
+		list.add("대전");
+		list.add("서울");
+		
+		list.forEach(tmp -> System.out.print(tmp  + ","));
+		
+		//add(index,value) => index 위치에 value 추가
+		list.add(1,"부산");	
+		System.out.println();
+		list.forEach(index -> System.out.print(index + ","));
+		
+		System.out.println();
+		
+		// get(index) index의 값 가져오기
+		System.out.println(list.get(1));
+		
+		// set(index,value) index의 값 변경
+		list.set(1,"부산변경");		
+		System.out.println(list.get(1));
+		
+		//indexOf(value) index 0부터 순서대로 검색 => 없는경우 -1
+		System.out.println(list.indexOf("서울"));				
+		//lastIndexOf(value) 반대 순서로 검색 => 없는경우 -1
+		System.out.println(list.lastIndexOf("서울"));			
+		System.out.println(list.lastIndexOf("대구") + "," + list.indexOf("대구"));			
+		
+		// remove(index)	index의 값 삭제
+		list.remove(0);				
+		// remove(value)	첫번째 검색 값 삭제
+		list.remove("서울");		 
+		
+		list.forEach(tmp -> System.out.println(tmp));
+		//저장되어 있는 값의 갯수
+		System.out.println(list.size());		
+		
+		//contains(value) : value 가 있는지 없는지 반환
+		String tmp = list.contains("부산변경") ? "포함" : "미포함";  
+		System.out.println(tmp);
+		
+		//ArrayList to Array
+		String[] arr  = list.toArray(new String[list.size()]);
+		for(String tmpArr : arr){
+			System.out.print(tmpArr + " ");
+		}
+		
+		//Array to List
+		List<String> list2 = Arrays.asList(arr);
+		list2.forEach(tmp3 -> System.out.print(tmp3 + " "));
+		
+		System.out.println();
+		ArrayList<String> list3 = new ArrayList<String>();
+		list3.add("test1");
+		list3.add("test2");
+		list3.add("test3");
+		//리스트의 값 모두 삭제
+		list3.clear();
+		//리스트가 비어있는지 확인
+		if(!list3.isEmpty()){	
+			list3.forEach(tmp3 -> System.out.print(tmp3 + " "));
+		}else{
+			System.out.println("비어있음");
+		}
+		
+		list3.add("test1");
+		//list1.addAll(list2) => list1, list2 를 합침
+		list.addAll(list2);
+		list.addAll(list3);
+		list.forEach(tmp3 -> System.out.print(tmp3 + " "));
+		
+		System.out.println();
+		ArrayList<String> list4 = new ArrayList<String>();
+		list4.add("부산변경");
+		list4.add("부산변경");
+		list4.add("대전");
+//		list4.add("서울");
+		list4.forEach(tmp3 -> System.out.print(tmp3 + " "));
+		
+		System.out.println();
+		//list1.containsAll(list2) => list2로 제공한 모든 값이 포함되어 있는지 여부를 반환
+		if(list.containsAll(list4)){
+			System.out.println("순서상관X");
+		}else{
+			System.out.println("순서상관O");
+		}
+		
+		
 		
 		/*
-		//for , 확장 for , forEach 
-		for(int i=0; i<vaList.getSize(); i++) {
-			ArrayList<String> inList = new ArrayList<String>();
-			inList = vaList.getListArr().get(i);
-			
-			//확장 for 문
-			for(String tmp : inList) {
-				System.out.println(tmp + " :: 확장 for");
-			}
-			
-			//forEach함수
-			inList.forEach(index -> System.out.println(index + " :: forEach"));
-			
-			System.out.println("==================================");
-		}
+		 HashMap<Integer, String> collect = list
+	                .stream()
+	                .collect(HashMap<Integer, String>::new,
+	                        (map, streamValue) -> map.put(map.size(), (String) streamValue),
+	                        (map, map2) -> {
+	                        });
+
+	        collect.forEach((k, v) -> System.out.println(k + ":" + v));
 		*/
-	
 	}
 
 }
