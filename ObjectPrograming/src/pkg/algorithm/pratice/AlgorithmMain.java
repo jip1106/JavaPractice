bec7296ccc258f5ee4eb4009b4544e69a618dd7c
Q220135 <Q220135@Q220135-PC>
2022-03-22 18:02:11 +0900

    커밋

---
+++ ObjectPrograming/src/pkg/algorithm/pratice/AlgorithmMain.java
@@ -0,0 +1,5 @@
+package pkg.algorithm.pratice;
+
+public class AlgorithmMain {
+
+}
--- ObjectPrograming/src/pkg/programers/week8/MainWeek8.java
+++ ObjectPrograming/src/pkg/programers/week8/MainWeek8.java
@@ -6,7 +6,18 @@ public class MainWeek8 {
 		// TODO Auto-generated method stub
 		//Week8Level1.numStringAndEnglishWord("one4seveneight");
 		
-		Week8Level1.minRectangle(new int[][]{{60,50}, {30,70}, {60,30}, {80,40} });
+		//Week8Level1.minRectangle(new int[][]{{60,50}, {30,70}, {60,30}, {80,40} });
+		
+		Week8Level1.dartGame("1S2D*3T");
+		/*
+		Week8Level1.dartGame("1D2S#10S");
+		Week8Level1.dartGame("1D2S0T");
+		Week8Level1.dartGame("1S*2T*3S");
+		Week8Level1.dartGame("1D#2S*3S");
+		Week8Level1.dartGame("1T2D3D#");
+		Week8Level1.dartGame("1D2S3T*");
+		*/
+		
 	}
 
 }
